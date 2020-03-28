f1f993f2e6e06974a0e7c5c6313206cc2e92b386
tmj <2327230612@qq.com>
2020-03-28 17:42:41 +0800

    添加maven项目

---
+++ maven01-start/src/test/java/cn/tmj/maven/test/MavenTest.java
@@ -0,0 +1,14 @@
+package cn.tmj.maven.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenTest {
+	@Test
+	public void testName() throws Exception {
+		System.out.println("lalal");
+		
+	}
+
+}
